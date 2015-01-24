5072ebb7a6ae9e497d6e79385b847d6d16cd74d9
Moncefmd <moncefmd@hotmail.com>
2015-01-23 21:27:09 -0500

    Enlevage du jumbotron

--- secrets.php
+++ secrets.php
@@ -18,15 +18,7 @@
 <!-- Rajouter le menu -->
 <?php include("menu.php"); ?>
 
-<div class="jumbotron">
-  <div class="container">
-    <h1>Hello, world!</h1>
 
-    <img src="img/background.jpg">
-    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.     </p>
-    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-  </div>
-</div>
 
 <div class="mainContent outsideMenu">
   <header>
