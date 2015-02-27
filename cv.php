3658c111ccebbc25f65ce7305fda0b732ceb6f09
Moncefmd <moncefmd@hotmail.com>
2015-02-27 15:31:05 -0500

    Changements dans la homepage et indentation

--- cv.php
+++ cv.php
@@ -1,70 +1,70 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Moncef|me - Curriculum Vitae</title>
-<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<!--[if lt IE 9]>
+        <title>Moncef|me - Curriculum Vitae</title>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <!--[if lt IE 9]>
 <script src="html5shiv/dist/html5shiv.js"></script>
 <![endif]-->
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-<!-- Rajouter le menu -->
-<?php include("menu.php"); ?>
-
-
-
-  <div class="mainContent outsideMenu">
-    <h1 class="text-center" id="titrePrincipal">Curriculum Vitae</h1>
-
-
-    <h1 class="text-center">Moncef Boudjani – moncefmd@hotmail.com</h1>
-    <ul>
-      <li>Français : parlé, écrit, langue maternelle, excellente maîtrise.</li>
-      <li>Anglais : parlé, écrit, langue seconde, excellente maîtrise.</li>
-    </ul>
-    <h1 class="text-center">Connaissances informatiques :</h1>
-    <br/>
-
-      <h6>Environnements :</h6> <p>Windows, Android, GNU/Linux</p>
-      <h6>Languages de programmation maitrisés :</h6>
-      <ul>
-        <li><strong>-	Excellent :</strong> Java, C++/C, Python et HTML 5</li>
-        <li><strong>-	Moyen :</strong> Assembleur, Scala</li>
-      </ul>
-        <h6>Outils communément utilisés :</h6> <p>Git, SVN, gcc, Qt, Unity.</p>
-        <strong>Autres :</strong> <p>Test Driven Developpement (TDD)</p>
-
-
-    <!-- Second part of the CV-->
-    <h1 class="text-center">Expériences de travail :</h1>
-      <ul>
-        <li><strong>Travail blablabla1 :</strong> .</li>
-        <li><strong>Travail blablabla2 :</strong> .</li>
-      </ul>
-      <h1 class="text-center">Experience universitaires :</h1>
-      <ul>
-        <li>-	<strong>Participation à la société technique universitaire PolyGames.</strong> Je suis dans cette société afin d’en apprendre plus sur le <strong>développement d’applications Android.</strong> On est une équipe de quatre et on doit développer un jeu de notre choix, le tout, entouré par un processus <strong>SCRUM.</strong> Le projet nous impose d’effectuer l’apprentissage de la plateforme Android la conception de notre jeu ainsi que la production de celui-ci. </li>
-      </ul>
-      <h1 class="text-center">Education :</h1>
-      <ul>
-        <li>-	<strong>Collège de Maisonneuve</strong>  -  Sciences de la nature et de la vie</li>
-        <li>-	<strong>École Polytechnique de Montréal</strong>  -  Génie Informatique</li>
-      </ul>
-
-</div>
+        <!-- Rajouter le menu -->
+        <?php include("menu.php"); ?>
 
 
 
+        <div class="mainContent outsideMenu">
+            <h1 class="text-center" id="titrePrincipal">Curriculum Vitae</h1>
 
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
+
+            <h1 class="text-center">Moncef Boudjani – moncefmd@hotmail.com</h1>
+            <ul>
+                <li>Français : parlé, écrit, langue maternelle, excellente maîtrise.</li>
+                <li>Anglais : parlé, écrit, langue seconde, excellente maîtrise.</li>
+            </ul>
+            <h1 class="text-center">Connaissances informatiques :</h1>
+            <br/>
+
+            <h6>Environnements :</h6> <p>Windows, Android, GNU/Linux</p>
+            <h6>Languages de programmation maitrisés :</h6>
+            <ul>
+                <li><strong>-	Excellent :</strong> Java, C++/C, Python et HTML 5</li>
+                <li><strong>-	Moyen :</strong> Assembleur, Scala</li>
+            </ul>
+            <h6>Outils communément utilisés :</h6> <p>Git, SVN, gcc, Qt, Unity.</p>
+            <strong>Autres :</strong> <p>Test Driven Developpement (TDD)</p>
+
+
+            <!-- Second part of the CV-->
+            <h1 class="text-center">Expériences de travail :</h1>
+            <ul>
+                <li><strong>Travail blablabla1 :</strong> .</li>
+                <li><strong>Travail blablabla2 :</strong> .</li>
+            </ul>
+            <h1 class="text-center">Experience universitaires :</h1>
+            <ul>
+                <li>-	<strong>Participation à la société technique universitaire PolyGames.</strong> Je suis dans cette société afin d’en apprendre plus sur le <strong>développement d’applications Android.</strong> On est une équipe de quatre et on doit développer un jeu de notre choix, le tout, entouré par un processus <strong>SCRUM.</strong> Le projet nous impose d’effectuer l’apprentissage de la plateforme Android la conception de notre jeu ainsi que la production de celui-ci. </li>
+            </ul>
+            <h1 class="text-center">Education :</h1>
+            <ul>
+                <li>-	<strong>Collège de Maisonneuve</strong>  -  Sciences de la nature et de la vie</li>
+                <li>-	<strong>École Polytechnique de Montréal</strong>  -  Génie Informatique</li>
+            </ul>
+
+        </div>
+
+
+
+
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Moncef|me - Home</title>
-<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<!--[if lt IE 9]>
+        <title>Moncef|me - Home</title>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <!--[if lt IE 9]>
 <script src="html5shiv/dist/html5shiv.js"></script>
 <![endif]-->
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-<body>
-<!-- Rajouter le menu -->
-<?php include("menu.php"); ?>
-<div class="outsideMenu">
-  <header>
-    <h1 class="text-center" id="titrePrincipal">Moncef<span class="yellow">.</span>me</h1>
-  </header>
-  <article class="text-center">
-    <p>I am a young student in engineering</p>
-    <p>Here is my <a href="cv.php">curriculum vitae</a></p>
-    <p>I am still working on this website</p>
-  </article>
- </div>
+        <body>
+            <!-- Rajouter le menu -->
+            <?php include("menu.php"); ?>
+            <div class="outsideMenu">
+                <header>
+                    <h1 class="text-center" id="titrePrincipal">Moncef<span class="yellow">.</span>me</h1>
+                </header>
+                <article class="text-center">
+                    <p>I am a young student in engineering</p>
+                    <p>Here is my <a href="cv.php">curriculum vitae</a></p>
+                    <p>I am still working on this website</p>
+                </article>
+            </div>
 
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
+            <script src="http://code.jquery.com/jquery-latest.js"></script>
+            <script src="js/bootstrap.js"></script>
+        </body>
 
 
 
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </body>
 </html>
 
